c6b87436caaec78c90fdc9c831b254dcd02503dc
Andy's Macbook <maxandersonmdr@gmail.com>
2026-05-12 04:06:18 +0100

    fix reconnect

--- test/test.dart
+++ test/test.dart
@@ -5,45 +5,40 @@ void _print(dynamic t, [String? message]) {
 }
 
 void main() async {
-  try {
-    MongoRealtime.connect('http://localhost:3000/ws', authData: "ok");
-    final users = realtime.collection("users");
+  MongoRealtime.connect('http://localhost:3000/ws', authData: "ok");
+  final users = realtime.collection("users", fromJson: (json) => json['email'].toString());
 
-    users
-        .where('age', isLowerThan: 10)
-        .stream
-        .listen(
-          (d) {
-            _print(d.length, '<10');
-          },
-          onError: (e) {
-            _print(e, 'error');
-          },
-        );
-    users
-        .where('age', isGreaterOrEqualTo: 5)
-        .stream
-        .listen(
-          (d) {
-            _print(d.length, '>=5');
-          },
-          onError: (e) {
-            _print(e, 'error');
-          },
-        );
-    users
-        .where('firstname', isEqualTo: 'Max')
-        .stream
-        .listen(
-          (d) {
-            _print(d.length, 'Max');
-          },
-          onError: (e) {
-            _print(e, 'error');
-          },
-        );
-    await Future.delayed(Duration(seconds: 2));
-  } on Exception catch (_) {
-    _print('add');
-  }
+  users
+      .where('age', isLowerThan: 10)
+      .streamWithValue
+      .listen(
+        (d) {
+          _print(d, '<10');
+        },
+        onError: (e) {
+          _print(e, 'error');
+        },
+      );
+  users
+      .where('age', isGreaterOrEqualTo: 5)
+      .streamWithValue
+      .listen(
+        (d) {
+          _print(d, '>=5');
+        },
+        onError: (e) {
+          _print(e, 'error');
+        },
+      );
+  users
+      .where('firstname', isEqualTo: 'Max')
+      .streamWithValue
+      .listen(
+        (d) {
+          _print(d, 'Max');
+        },
+        onError: (e) {
+          _print(e, 'error');
+        },
+      );
 }
